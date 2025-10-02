cedd0aeb70bed9a824a578929c62b2e0c574bd73
carolinagiannasca <cgiannasca@fi.uba.ar>
2025-10-02 09:02:09 -0300

    cambios

--- tdas/lista/lista_enlazada.go
+++ tdas/lista/lista_enlazada.go
@@ -38,50 +38,64 @@ func (lista *listaEnlazada[T]) EstaVacia() bool {
 }
 
 func (lista *listaEnlazada[T]) InsertarPrimero(dato T) {
+
 	nuevoPrimero := crearNodoLista(dato)
+
 	if lista.EstaVacia() {
 		lista.ultimo = nuevoPrimero
 	}
+
 	nuevoPrimero.siguiente = lista.primero
 	lista.primero = nuevoPrimero
 	lista.largo++
 }
 
 func (lista *listaEnlazada[T]) InsertarUltimo(dato T) {
+
 	nuevoUltimo := crearNodoLista(dato)
+
 	if lista.EstaVacia() {
 		lista.primero = nuevoUltimo
 	} else {
 		lista.ultimo.siguiente = nuevoUltimo
 	}
+
 	lista.ultimo = nuevoUltimo
 	lista.largo++
 }
 
 func (lista *listaEnlazada[T]) BorrarPrimero() T {
+
 	if lista.EstaVacia() {
 		panic(MSG_LISTA_VACIA)
 	}
+
 	nodo := lista.primero
 	lista.primero = nodo.siguiente
+
 	if lista.primero == nil {
 		lista.ultimo = nil
 	}
+
 	lista.largo--
 	return nodo.dato
 }
 
 func (lista *listaEnlazada[T]) VerPrimero() T {
+
 	if lista.EstaVacia() {
 		panic(MSG_LISTA_VACIA)
 	}
+
 	return lista.primero.dato
 }
 
 func (lista *listaEnlazada[T]) VerUltimo() T {
+
 	if lista.EstaVacia() {
 		panic(MSG_LISTA_VACIA)
 	}
+
 	return lista.ultimo.dato
 }
 
@@ -90,7 +104,9 @@ func (lista *listaEnlazada[T]) Largo() int {
 }
 
 func (lista *listaEnlazada[T]) Iterar(visitar func(T) bool) {
+
 	actual := lista.primero
+
 	for actual != nil {
 		if !visitar(actual.dato) {
 			break
@@ -107,9 +123,11 @@ func (lista *listaEnlazada[T]) Iterador() IteradorLista[T] {
 }
 
 func (iter *iterListaEnlazada[T]) VerActual() T {
+
 	if !iter.HaySiguiente() {
 		panic(MSG_ITER_TERMINADA)
 	}
+
 	return iter.actual.dato
 }
 
@@ -118,42 +136,53 @@ func (iter *iterListaEnlazada[T]) HaySiguiente() bool {
 }
 
 func (iter *iterListaEnlazada[T]) Siguiente() {
+
 	if !iter.HaySiguiente() {
 		panic(MSG_ITER_TERMINADA)
 	}
+
 	iter.anterior = iter.actual
 	iter.actual = iter.actual.siguiente
 }
 
 func (iter *iterListaEnlazada[T]) Insertar(dato T) {
+
 	nuevoNodo := crearNodoLista(dato)
 	nuevoNodo.siguiente = iter.actual
+
 	if iter.anterior == nil {
 		iter.lista.primero = nuevoNodo
 	} else {
 		iter.anterior.siguiente = nuevoNodo
 	}
-	if iter.actual == nil {
+
+	if !iter.HaySiguiente() {
 		iter.lista.ultimo = nuevoNodo
 	}
+
 	iter.actual = nuevoNodo
 	iter.lista.largo++
 }
 
 func (iter *iterListaEnlazada[T]) Borrar() T {
+
 	if !iter.HaySiguiente() {
 		panic(MSG_ITER_TERMINADA)
 	}
+
 	nodoActual := iter.actual
 	if iter.anterior == nil {
 		iter.lista.primero = nodoActual.siguiente
 	} else {
 		iter.anterior.siguiente = nodoActual.siguiente
 	}
+
 	if iter.actual.siguiente == nil {
 		iter.lista.ultimo = iter.anterior
 	}
+
 	iter.actual = iter.actual.siguiente
 	iter.lista.largo--
+
 	return nodoActual.dato
 }
